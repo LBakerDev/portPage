639b75990e15f97c68c971df8b2e3f1760f6aaa0
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-07-27 03:27:36 -0400

    completed home, projects and articles page

---
+++ index.js
@@ -0,0 +1,30 @@
+const bodyParser    = require('body-parser');
+const express       = require("express");
+const app           = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+    res.redirect("home");
+});
+
+app.get("/home", function (req, res) {
+    res.render("home");
+});
+
+app.get("/home/articles", function(req, res) {
+    res.render("articles");
+})
+
+app.get("/home/projects", function (req, res) {
+    res.render("projects");
+})
+
+
+
+
+app.listen(8080 || process.env.Port, function () {
+    console.log('Server is running mate!!');
+});
